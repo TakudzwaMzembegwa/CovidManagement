deb85e9b048b9de27e74a8ad9cab261cf502533e
Takudzwa Mzembegwa <38813522+TakudzwaMzembegwa@users.noreply.github.com>
2022-05-17 02:21:18 +0200

    Update db.py

--- REST API/db.py
+++ REST API/db.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_pymongo import pymongo
 import certifi
 
-CONNECTION_STRING = "mongodb+srv://CSC735IoT:jwZwjhZvgW308qCq@cluster0.yfejg.mongodb.net/CovidManagement?retryWrites=true&w=majority"
+CONNECTION_STRING = ""
 client = pymongo.MongoClient(CONNECTION_STRING, tlsCAFile=certifi.where())
 db = client.get_database("CovidManagement")
 user_db = pymongo.collection.Collection(db, 'User')
